66003d80b00512f9584473f99cad016f6d4545e4
krbrigi <krbrigi@gmail.com>
2026-06-17 21:13:31 +0200

    Extend cart tests with continue shopping flow

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -13,3 +13,14 @@ class TestCart():
         cart_page.remove_product_from_cart("sauce-labs-backpack")
 
         assert cart_page.get_cart_badge_count() == 0
+
+    def test_continue_shopping(self, driver, logged_in):
+        logged_in("standard_user", PASSWORD)
+        inventory_page = InventoryPage(driver)
+        cart_page = CartPage(driver)
+
+        inventory_page.open_cart()
+        cart_page.continue_shopping()
+
+        assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+
